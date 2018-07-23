c66dbdda9a9deeb55abf6272f71a027b76ed61b8
prashantgangwar <prashant.enbake@gmail.com>
2018-07-23 12:56:52 +0530

    identation done

---
+++ js/assingment_js.js
@@ -0,0 +1,21 @@
+	$("#video1, #video2, #video3, #video4").videoPopup({
+
+  // autoplay on open
+  autoplay: true,
+
+  // shows video controls
+  showControls: true,
+
+  // colors of controls
+  controlsColor: null,
+
+  // infinite loop
+  loopVideo: true,
+
+  // shows video information
+  showVideoInformations: true,
+
+  // width
+  width: null
+  
+});
